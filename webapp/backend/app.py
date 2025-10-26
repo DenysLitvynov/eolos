e20c465336d976e150a3a9d943d69952c1a82b79
Denys Litvynov <denislit741@gmail.com>
2025-10-26 11:07:07 +0100

    Servidor API REST implementado, versión 1.

---
+++ webapp/backend/app.py
@@ -0,0 +1,47 @@
+"""
+Autor: Denys Litvynov Lymanets
+Fecha: 26-10-2025
+Descripción: Archivo principal de la aplicación FastAPI. Configuramos app, middleware, rutas.
+"""
+
+# ---------------------------------------------------------
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
+from .api.auth import router as auth_router  # Agregado para auth
+from .db.database import engine
+from .db.models import Base
+
+# ---------------------------------------------------------
+
+app = FastAPI(title="API REST para Proyecto Biometría y Medio Ambiente", version="1.0.0")
+
+# Crear tablas si no existen 
+Base.metadata.create_all(bind=engine)
+
+# Routers
+app.include_router(auth_router, prefix="/api/v1")
+
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Archivos estáticos
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
+app.mount("/js", StaticFiles(directory=FRONTEND_DIR / "js"), name="js")
+app.mount("/css", StaticFiles(directory=FRONTEND_DIR / "css"), name="css")
+
+@app.get("/")
+async def root():
+    return FileResponse(FRONTEND_DIR / "index.html")
+
+# ---------------------------------------------------------
