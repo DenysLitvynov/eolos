ae68569612dc9484c2af17da44f976e753543ae5
Juan Bta. Peris Fontstad <jbperfon@epsg.upv.es>
2025-11-16 20:11:40 +0100

    Implementación de hoja de estilos y actualización de recursos

---
+++ android/eolos/app/src/main/java/com/example/eolos/activities/Home_UR.java
@@ -0,0 +1,15 @@
+package com.example.eolos.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.eolos.R;
+
+public class Home_UR extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page_usuario_registrado); // el que hemos ido tocando
+    }
+}
