e0f8763dface98c968242537f19ed8acd9a0a456
Denys Litvynov <denislit741@gmail.com>
2025-12-12 10:40:47 +0100

    Ip del servidor modificada en los ficheros del android

--- android/eolos/app/src/main/java/com/example/eolos/logica_fake/RegistroFake.java
+++ android/eolos/app/src/main/java/com/example/eolos/logica_fake/RegistroFake.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
 
 public class RegistroFake {
 
-    private static final String BASE_URL = "http://192.168.1.133:8000";
+    private static final String BASE_URL = "http://51.95.74.33:8000";
     private static final String ENDPOINT_REGISTRO = "/api/v1/auth/registro";
 
     /**
--- android/eolos/app/src/main/java/com/example/eolos/logica_fake/VerifyFake.java
+++ android/eolos/app/src/main/java/com/example/eolos/logica_fake/VerifyFake.java
@@ -17,7 +17,7 @@ import org.json.JSONObject;
 
 public class VerifyFake {
 
-    private static final String BASE_URL = "http://192.168.1.133:8000";
+    private static final String BASE_URL = "http://51.95.74.33:8000";
 
     public interface VerifyCallback {
         void onSuccess(String token);
